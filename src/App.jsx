15ea9962cea48edb285c98b529914c502435e994
PanaxTab <panax36@gmail.com>
2024-08-21 09:47:36 +0300

    Created a basic NotesPage and loaded in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import NotesPage from "./pages/NotesPage.jsx";
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div id="app">
+        <NotesPage />
+      </div>
     </>
   )
 }
