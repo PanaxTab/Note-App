a5ea00f49b374e8627a7f18b31386297c46af5c7
PanaxTab <panax36@gmail.com>
2024-08-21 09:57:19 +0300

    model how database will store data and how it's written

---
+++ src/assets/FakeData.js
@@ -0,0 +1,35 @@
+export const fakeData = [
+	{
+		$id: 1,
+		body: JSON.stringify(),
+		colors: JSON.stringify({
+			id:"color-purple",
+			colorHeader: "#FED0FD",
+			colorBody: "#FEE5FD",
+			colorText: "#18181A",
+		}),
+		position: JSON.stringify({x:505,y:10}),
+	},
+	{
+		$id: 2,
+		body: JSON.stringify(),
+		colors: JSON.stringify({
+			id:"color-purple",
+			colorHeader: "#9BD1DE",
+			colorBody: "#A6DCE9",
+			colorText: "#18181A",
+		}),
+		position: JSON.stringify({x:305,y:110}),
+	},
+	{
+		$id: 3,
+		body: JSON.stringify(),
+		colors: JSON.stringify({
+			id:"color-purple",
+			colorHeader: "#FFEFBE",
+			colorBody: "#FFF5DF",
+			colorText: "#18181A",
+		}),
+		position: JSON.stringify({x:605,y:500}),
+	},
+];
