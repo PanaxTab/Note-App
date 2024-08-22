708ca228a98c73869a93a2187dc25187a77cf36a
PanaxTab <panax36@gmail.com>
2024-08-22 08:02:44 +0300

    changed body styling, added trash icon, made every card have it's own positioning

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,11 +1,22 @@
+import Trash from "../icons/Trash.jsx"
 const NoteCard = ({note}) => {
   let position = JSON.parse(note.position);
   const colors = JSON.parse(note.colors);
   const body = JSON.parse(note.body);
   
   return (
-  <div className="card" style={backgroundColor:color.colorBody,}>
-    {body}
+  <div className="card" style={{backgroundColor:color.colorBody,}}>
+    <div className="card-header" style={{
+        backgroundColor:colors.colorHeader,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}>
+        <Trash />
+    </div>
+    <div className="card-body">
+      <textarea style={{color:colors.colorText}} defeaultValue={body}>
+      </textarea>
+    </div>
   </div>
   );
 }
