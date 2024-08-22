ccae4b5bca50c8b900299d3d238733dd281f93e7
PanaxTab <panax36@gmail.com>
2024-08-22 09:04:58 +0300

    fixed exports, added dynamic card positions

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,4 +1,23 @@
 import Trash from "../icons/Trash.jsx"
+
+useEffect(() => {
+  autoGrow(textAreaRef);
+}, [])
+
+//Changes position of a note from a static variable to a static hook
+const [position,setPosition] = useState(JSON.parase(note.position));
+
+//Starting position of where the mouse clicked and then moved
+let mouseStartPos = {x:0,y:0};
+const cardRef = useRef(null);
+
+function autoGrow(textAreaRef){
+  const {current} = textAreaRef;
+  current.style.height = "auto";
+  current.style.height = scrollHeight + "px";
+}
+
+const textAreaRef = useRef(null);
 const NoteCard = ({note}) => {
   let position = JSON.parse(note.position);
   const colors = JSON.parse(note.colors);
@@ -14,9 +33,17 @@ const NoteCard = ({note}) => {
         <Trash />
     </div>
     <div className="card-body">
-      <textarea style={{color:colors.colorText}} defeaultValue={body}>
+      <textarea 
+        style={{color:colors.colorText}} 
+        defeaultValue={body} 
+        ref={textAreaRef}>
+        onInput={()=>{
+          autoGrow(textAreaRef);
+        }}
       </textarea>
     </div>
   </div>
   );
 }
+
+export default Notecard;
